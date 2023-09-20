d75c88846d99963709b0f00fe063f7978b1ed327
josiascastro <josiascastro2003@gmail.com>
2023-09-20 14:29:08 -0300

    add head.php

---
+++ components/layout/head.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Screen Size, Distance, and Resolution Calculator</title>
+    <!-- <link rel="stylesheet" href="/public/css/flowbite.min.css"> -->
+    <link rel="stylesheet" href="/public/css/tailwind.min.css">
+    <link rel="stylesheet" href="/public/css/styles.css">
+    <script src="/public/js/vue.js"></script>
+</head> 
