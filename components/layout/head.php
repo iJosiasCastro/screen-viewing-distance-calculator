ed06c03682909164948ce635fca613aaf793a9b5
josiascastro <josiascastro2003@gmail.com>
2023-09-22 15:59:50 -0300

    add fontawesome icons

--- components/layout/head.php
+++ components/layout/head.php
@@ -1,9 +1,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Screen Size, Distance, and Resolution Calculator</title>
+    <title>Screen Siting Distance Calculator</title>
     <!-- <link rel="stylesheet" href="/public/css/flowbite.min.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/public/css/tailwind.min.css">
     <link rel="stylesheet" href="/public/css/styles.css">
     <script src="/public/js/vue.js"></script>
+    <script src="/script/sitingDistanceGraph.js"></script>
 </head> 
