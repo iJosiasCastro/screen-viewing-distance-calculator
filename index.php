192d3985f405666748d72d719743fb51ff3c2397
josiascastro <josiascastro2003@gmail.com>
2023-09-23 12:42:48 -0300

    update design and info in index.php

--- index.php
+++ index.php
@@ -5,44 +5,71 @@
 <body class="bg-gray-100 py-8">
     <h1 class="text-2xl font-semibold mb-4 px-2 mx-auto max-w-4xl">Screen Viewing Distance Calculator</h1>
     <div class="container mx-auto max-w-4xl bg-white p-8 rounded shadow-lg">
-        <div id="calculatorSection" style="display: non;">
+        <div id="calculatorSection">
             <?php include('./components/form.php') ?>
         </div>
         <div id="resultSection" style="display: none;">
-            <button class="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-3 py-2 rounded flex items-center" onclick="resetCalculator()">
-                <i class="fas fa-chevron-left mr-2"></i>
-                Back to Calculator
-            </button>
-            <div id="resultSpecs" class="mt-4 flex w-full">
-                
-
+            <div class="flex gap-3">
+                <button class="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-3 py-2 rounded flex items-center" onclick="resetCalculator()">
+                    <i class="fas fa-chevron-left mr-2"></i>
+                    Back to Calculator
+                </button>
+                <button class="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-3 py-2 rounded flex items-center" onclick="calculate()">
+                    <i class="fas fa-refresh mr-2"></i>
+                    Refresh results
+                </button>
             </div>
-            
-            <div class="grid grid-cols-3 gap-4 my-5 py-3 border-t border-b">
-                <div class="border-r">
-                    <h4 class="font-semibold mb-2">Minimum distance</h4>
-                    <?php
-                        $id = "minimumDistance";
-                        include('./components/sitingDistanceGraph.php');
-                    ?>
-                </div>
-                <div class="border-r">
-                    <h4 class="font-semibold mb-2">Maximum distance</h4>
-                    <?php
-                        $id = "maximumDistance";
-                        include('./components/sitingDistanceGraph.php');
-                    ?>
-                </div>
-                <div>
-                    <h4 class="font-semibold mb-2">Visual Acuity distance</h4>
-                    <?php
-                        $id = "visualAcuityDistance";
-                        include('./components/sitingDistanceGraph.php');
-                    ?>
+            <div class="overflow-x-auto max-w-full">
+                <div style="min-width: 700px;">
+                    <div id="resultSpecs" class="mt-4 flex w-full"></div>
+                    <div class="grid grid-cols-3 gap-4 my-5 py-3 border-t border-b">
+                        <div class="border-r">
+                            <div class="flex items-center">
+                                <h4 class="font-semibold mb-2">Minimum distance</h4>
+                                <?php
+                                    $modalTitle = "Minimum distance";
+                                    $modalContent = '<div id="note-107-1"><strong>The audience should sit&nbsp;at least this distance from the screen</strong>.<br> This is the <em>Shortest Recommended Viewing Distance</em> based on Field-of-View being too wide: This distance is calculated&nbsp;on the peripheral vision field&nbsp;of view of the human eye. The average FOV&nbsp;width for the human eye is 140 degrees. The rule is that if the viewer sits any closer than this distance to the screen and looks at one side of the screen, they will not be able to see the other side of the screen with their peripheral vision. This equates to a 70-degree field of view when the person is looking at the center of the screen.</div>';
+                                    include('./components/infoModal.php');
+                                ?>
+                            </div>
+                            <?php
+                                $id = "minimumDistance";
+                                include('./components/sitingDistanceGraph.php');
+                            ?>
+                        </div>
+                        <div class="border-r">
+                            <div class="flex items-center">
+                                <h4 class="font-semibold mb-2">Maximum distance</h4>
+                                <?php
+                                    $modalTitle = "Maximum distance";
+                                    $modalContent = '<div><strong>The audience should sit at most&nbsp;this far&nbsp;from&nbsp;the screen</strong>. <br> This is based on THX&nbsp;<em>Longest Recommended</em> and <em>Longest Allowable</em> viewing distances: THX publishes standards to which movie theaters must adhere to receive THX certification. &nbsp;THX recommends that the back row of seats in a theater have a 36 degree or greater viewing angle and requires a minimum of a 26 degree or greater viewing angle to receive certification. (Note: sitting closer to the screen results in a wider field-of-view.)</div>';
+                                    include('./components/infoModal.php');
+                                ?>
+                            </div>
+                            <?php
+                                $id = "maximumDistance";
+                                include('./components/sitingDistanceGraph.php');
+                            ?>
+                        </div>
+                        <div>
+                            <div class="flex items-center">
+                                <h4 class="font-semibold mb-2">Visual Acuity distance</h4>
+                                <?php
+                                    $modalTitle = "Visual Acuity distance";
+                                    $modalContent = '<div><strong>The Visual Acuity Distance</strong> based on <em>Visual Acuity.&nbsp;</em><br> This distance is calculated based on the reference resolving power of the eyes. The human eye with 20/20 vision can detect or resolve details as small as 1/60th of a degree of arc. This distance represents the point beyond which some details in the picture are no longer able to be resolved, so pixels begin to blend together. Closer to the screen than this may result in the need for higher resolution display. This value should&nbsp;be lowered if visual acuity is worst then 20/20, raised if visual acuity is better.</div>';
+                                    include('./components/infoModal.php');
+                                ?>
+                            </div>
+                            <?php
+                                $id = "visualAcuityDistance";
+                                include('./components/sitingDistanceGraph.php');
+                            ?>
+                        </div>
+                    </div>
                 </div>
             </div>
             <small>
-                The information is accurate to 99%. It is not recommended for commercial purposes.
+                The information is not 100% accurate. It is not recommended for commercial purposes.
             </small>
             
         </div>
@@ -89,8 +116,6 @@
             // Visual Acuity distance
             const visualAcuityDistanceAngle = width / 60;
 
-            // console.log(visualAcuityDistanceAngle)
-
             createGraph('minimumDistance', minimumDistanceAngle, [screenWidthInches, screenWidthCm]);
             createGraph('maximumDistance', maximumDistanceAngle, [screenWidthInches, screenWidthCm]);
             createGraph('visualAcuityDistance', visualAcuityDistanceAngle, [screenWidthInches, screenWidthCm]);
@@ -99,12 +124,12 @@
             const result = `
                 <div class="w-full">
                     <h3 class="text-2xl mb-2">Display details:</h3>
-                    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:gap-x-2">
+                    <div class="grid grid-cols-3 gap-x-2">
                         <div>
                             <span class="font-semibold">Screen Diagonal:</span> ${diagonal.toFixed(1)} ${unit}<br>
                             <span class="font-semibold">Screen Resolution:</span> ${width} x ${height}<br>
                         </div>
-                        <div class="row-start-2 md:row-start-1">
+                        <div>
                             <span class="font-semibold">Screen Width:</span> ${screenWidthInches.toFixed(1)}" (${screenWidthCm.toFixed(1)}cm)<br>
                             <span class="font-semibold">Screen Height:</span> ${screenHeightInches.toFixed(1)}" (${screenHeightCm.toFixed(1)}cm)<br>
                         </div>
@@ -132,7 +157,8 @@
             document.getElementById("calculatorSection").style.display = '';
         }
     </script>
-
+    <script src="/public/js/flowbite.min.js"></script>
+    
 </body>
 
 </html>
